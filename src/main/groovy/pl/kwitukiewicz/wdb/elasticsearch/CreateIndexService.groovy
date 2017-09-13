a534b4d4cbd94c5619b28e431617498ab7823898
Krzysztof Witukiewicz <krzysztof1980@users.noreply.github.com>
2017-09-13 18:55:55 +0200

    Switch to ES bulk API
    (again, but this time with ReST client)

--- src/main/groovy/pl/kwitukiewicz/wdb/elasticsearch/CreateIndexService.groovy
+++ src/main/groovy/pl/kwitukiewicz/wdb/elasticsearch/CreateIndexService.groovy
@@ -8,18 +8,27 @@ import org.apache.http.HttpStatus
 import org.apache.http.auth.AuthScope
 import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.client.CredentialsProvider
-import org.apache.http.entity.ByteArrayEntity
 import org.apache.http.entity.ContentType
 import org.apache.http.impl.client.BasicCredentialsProvider
-import org.apache.http.message.BasicHeader
 import org.apache.http.nio.entity.NStringEntity
+import org.elasticsearch.action.DocWriteRequest
+import org.elasticsearch.action.bulk.BulkProcessor
+import org.elasticsearch.action.bulk.BulkRequest
+import org.elasticsearch.action.bulk.BulkResponse
+import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.ResponseException
 import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.unit.ByteSizeUnit
+import org.elasticsearch.common.unit.ByteSizeValue
+import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.threadpool.ThreadPool
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import pl.kwitukiewicz.wdb.model.WiktionaryPageDocument
 
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
@@ -33,22 +42,56 @@ class CreateIndexService {
     private static final Logger logger = LoggerFactory.getLogger(CreateIndexService.class)
     private static final Logger errorLogger = LoggerFactory.getLogger("indexation_errors")
 
-    private final RestClient client
+    private final RestClient lowLevelClient
+    private final RestHighLevelClient client
+    private final ThreadPool threadPool
+    private final BulkProcessor bulkProcessor
     private final ObjectMapper objectMapper
 
-    private final AtomicInteger indexationCounter = new AtomicInteger()
-    private final Map<String, Exception> indexationExceptions = new ConcurrentHashMap<>()
+    private final AtomicInteger indexingCounter = new AtomicInteger()
+
+    private final BulkProcessor.Listener bulkProcessorListener = new BulkProcessor.Listener() {
+
+        @Override
+        void beforeBulk(long executionId, BulkRequest request) {
+            // do nothing
+        }
+
+        @Override
+        void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+            if (response.hasFailures()) {
+                errorLogger.error(response.buildFailureMessage())
+            } else {
+                int currCount = indexingCounter.addAndGet(request.numberOfActions())
+                if (currCount % 10000 == 0) {
+                    logger.info("Indexed ${currCount} documents so far!")
+                }
+            }
+        }
+
+        @Override
+        void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+            errorLogger.error("Exception was thrown during bulk execution " + executionId, failure)
+        }
+    }
 
     private CreateIndexService() {
-        client = RestClient.builder(new HttpHost("localhost", 9200, "http"))
-                           .setHttpClientConfigCallback({httpClientBuilder ->
+        lowLevelClient = RestClient.builder(new HttpHost("localhost", 9200, "http"))
+                                   .setHttpClientConfigCallback({ httpClientBuilder ->
             CredentialsProvider credentialsProvider = new BasicCredentialsProvider()
             credentialsProvider.setCredentials(AuthScope.ANY,
                                                new UsernamePasswordCredentials("elastic",
                                                                                "changeme"))
             return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
         })
-                           .build()
+                                   .build()
+        client = new RestHighLevelClient(lowLevelClient)
+        threadPool = new ThreadPool(Settings.builder().put("node.name", "dummy").build())
+        bulkProcessor = new BulkProcessor.Builder({ r, l -> client.bulkAsync(r, l) },
+                                                  bulkProcessorListener,
+                                                  threadPool)
+                .setBulkSize(new ByteSizeValue(15, ByteSizeUnit.MB))
+                .build()
         objectMapper = new ObjectMapper()
     }
 
@@ -65,39 +108,36 @@ class CreateIndexService {
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException("Error indexing " + document, e)
         }
-        String endpoint = "/${INDEX}/${TYPE}/${id}"
         byte[] body
         try {
             body = objectMapper.writeValueAsBytes(document)
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e)
         }
-        ByteArrayEntity entity = new ByteArrayEntity(body)
-        try {
-            client.performRequest("PUT", endpoint, Collections.emptyMap(), entity,
-                                  new BasicHeader("Content-Type", ContentType.APPLICATION_JSON.toString()))
-            indexationCounter.incrementAndGet()
-        } catch (Exception e) {
-            indexationExceptions.put(id, e)
-        }
-        def count = indexationCounter.get() + indexationExceptions.size()
-        if (count % 1000 == 0) {
-            logger.info("Indexed ${count} documents so far!")
-        }
+        bulkProcessor.add(
+                new IndexRequest(INDEX, TYPE, id)
+                        .opType(DocWriteRequest.OpType.CREATE)
+                        .source(body, XContentType.JSON))
     }
 
     void cleanup() {
         resetRefreshInterval()
-        logger.info("The indexation was finished! Successfully indexed {} documents. There were {} errors!",
-                    indexationCounter.get(), indexationExceptions.size())
-        if (!indexationExceptions.isEmpty()) {
-            errorLogger.error("There were {} errors during indexation! The errors are:", indexationExceptions.size())
-            indexationExceptions.forEach({id, e -> errorLogger.error("Exception indexing document id=${id}", e)})
-        }
+        logger.info("Cleaning up after indexing...")
         try {
-            client.close()
-        } catch (IOException e) {
-            logger.error("Error closing client!", e)
+            if (bulkProcessor.awaitClose(5, TimeUnit.MINUTES)) {
+                logger.info("All bulk requests finished.")
+            } else {
+                logger.warn("There were unfinished bulk requests, consider increasing ttl.")
+            }
+        } catch (InterruptedException e) {
+            logger.error("Exception while waiting for bulk operation to finish", e)
+        } finally {
+            bulkProcessor.close()
+            if (!ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS)) {
+                logger.warn("ThreadPool termination timed out!")
+            }
+            lowLevelClient.close()
+            logger.info("Indexing was finished - ${indexingCounter.get()} documents were indexed!")
         }
     }
 
@@ -108,7 +148,7 @@ class CreateIndexService {
 
     private void deleteIndex() {
         try {
-            client.performRequest("DELETE", "/" + INDEX)
+            lowLevelClient.performRequest("DELETE", "/" + INDEX)
             logger.info("Deleted index ${INDEX}")
         } catch (ResponseException e) {
             if (e.getResponse().getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND) {
@@ -126,7 +166,7 @@ class CreateIndexService {
 {
     "settings": {
         "index": {
-            "number_of_shards": "1",
+            "number_of_shards": "5",
             "number_of_replicas": "0",
             "refresh_interval": "-1"
         }
@@ -155,7 +195,7 @@ class CreateIndexService {
 }"""
         HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON)
         try {
-            client.performRequest("PUT", INDEX, Collections.emptyMap(), entity)
+            lowLevelClient.performRequest("PUT", INDEX, Collections.emptyMap(), entity)
             logger.info("Created index ${INDEX}")
         } catch (Exception e) {
             throw new RuntimeException("Error creating index " + INDEX, e)
@@ -173,7 +213,7 @@ class CreateIndexService {
 }"""
         HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON)
         try {
-            client.performRequest("PUT", INDEX + "/_settings", Collections.emptyMap(), entity)
+            lowLevelClient.performRequest("PUT", INDEX + "/_settings", Collections.emptyMap(), entity)
         } catch (Exception e) {
             logger.error("Error setting refresh_interval to default value", e)
         }
